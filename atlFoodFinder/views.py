10d63fefdaafe71a497d7ec7fac9c34d1f0dfe9f
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-09-18 22:12:14 -0400

    adding map page url

--- atlFoodFinder/views.py
+++ atlFoodFinder/views.py
@@ -4,16 +4,17 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 
 # Create your views here.
-
 def create_account(request):
     return render(request, 'atlFoodFinder/create_account.html')
-
 def profile_page(request):
     return render(request, 'atlFoodFinder/profile_page.html')
 def favorites(request):
     return render(request, 'atlFoodFinder/favorites.html')
 
-def create_account(request):
+def show_map(request):
+    return render(request, 'atlFoodFinder/show_map.html')
+
+def create_account_action(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
