fdd1acf6a7f5203cdd50ead6d163957602f92a93
mayazhang <mayazhang1241@gmail.com>
2024-09-21 22:08:54 -0400

    redid login/create account

--- atlFoodFinder/views.py
+++ atlFoodFinder/views.py
@@ -3,17 +3,55 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import logout
 
 # Create your views here.
+
+def logout_user(request):
+    logout(request)
+    return redirect('atlFoodFinder:login_user')
+
+@login_required
+def profile_page(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password has been successfully updated!')
+            return redirect('atlFoodFinder:profile_page')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    return render(request, 'atlFoodFinder/profile_page.html',{
+        'form': form,
+        'user': request.user,
+    })
+
 def create_account(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+        print(f"First Name: {first_name}, Last Name: {last_name}")
 
         # Create a new user
         try:
-            user = User.objects.create_user(username=username, password=password, email=email)
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name
+            )
             user.save()
             login(request, user)  # Automatically log in the user
             return redirect('atlFoodFinder:show_map')  # Redirect to show_map page
@@ -22,8 +60,6 @@ def create_account(request):
 
     return render(request, 'atlFoodFinder/create_account.html')
 
-def profile_page(request):
-    return render(request, 'atlFoodFinder/profile_page.html')
 def favorites(request):
     return render(request, 'atlFoodFinder/favorites.html')
 
